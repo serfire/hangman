490c400d01b742e93e7cb27ee7055a2e1939252a
serfire <serfire@gmail.com>
2013-09-27 15:27:59 +0800

    TODO

--- http-facility.lisp
+++ http-facility.lisp
@@ -2,13 +2,13 @@
 (ql:quickload :cl-json)
 (ql:quickload :drakma)
 
+(in-package :hangman)
+
+(export '(initite-game))
+
 (defparameter *game-url* "http://strikingly-interview-test.herokuapp.com/guess/process")
 
-(defvar *req* '((date . "20071001") (time . "00") (origin . "all")))
-
-(format t "json:~S~%" (json:encode-json-to-string *req*))
-
-(defparameter *user-id* nil)
+(defparameter *user-id* "shilei.wang.1981@gmail.com")
 
 (defparameter *secret* nil)
 
@@ -18,5 +18,43 @@
       (null (cdr entity)))
     (list `("action" . ,action) `("guess" . ,guess) `("userId". ,*user-id*) `("secret" . ,*secret*))))
 
+(defun retrieve (request) 
+  (let ((stream (drakma:http-request *game-url* 
+                                     :accept "application/json"
+                                     :method :post
+                                     :content-type "application/json"
+                                     :external-format-in :utf-8
+                                     :external-format-out :utf-8
+                                     :content (json:encode-json-to-string request)
+                                     :want-stream t)))
+    (progn 
+      (st-json:read-json stream))))
+
+(defun initite-game ()
+  (let ((response (retrieve (request-json-string :action "initiateGame"))))
+    (if (= (st-json:getjso "status" response) 200)
+      (setf *secret* (st-json:getjso "secret" response))
+      nil)))
+
+(defun give-me-a-word ()
+  (let ((response (retrieve (request-json-string :action "nextWord"))))
+    (parse-response-word response)))
+
+(defun make-a-guess (guess)
+  (let ((response (retrieve (request-json-string :action "guessWord" :guess guess))))
+    (parse-response-word response)))
+
+(defun parse-response-word (response)
+  (if (= (st-json:getjso "status" response) 200)
+    (concatenate 'string "\^" (map 
+                                'string 
+                                (lambda (ch) 
+                                  (if (char= ch #\*)
+                                    #\.
+                                    ch))
+                                (st-json:getjso "word" response)) "\$")
+    nil))  
 
 
+(defun play-a-round()
+  nil)
---
+++ word-guess.lisp
@@ -0,0 +1,51 @@
+(load "~/quicklisp/setup.lisp")
+(ql:quickload "cl-ppcre")
+
+(in-package :hangman)
+(export '(load-words
+          most-likely-to-be))
+
+(defun load-words (path) 
+  (let ((word-repo nil))
+    (progn (with-open-file (words (make-pathname :name path) :direction :input )
+             (do 
+               ((i 0 (+ i 1))
+                (word (read-line words nil 'eof) (read-line words nil 'eof)))
+               ((eql word 'eof))
+               (push (cons i word) word-repo)))
+           word-repo)))
+
+(defun filter-all (candidates pattern)
+  (remove-if-not 
+    (lambda (c) 
+      (if (cl-ppcre:scan pattern (cdr c) :start 0 :end (- (length pattern) 2))
+        t
+        nil))
+    (remove-if-not 
+      (lambda (c) 
+        (= (length (cdr c)) (-  (length pattern) 2))) 
+      candidates)) )
+
+(defun all-letter-rates (candidates) 
+  (let ((repo (make-hash-table)))
+    (progn
+      (mapcar 
+        (lambda (c)
+          (progn (loop for ch across (cdr c) do
+                       (if (gethash ch repo)
+                         (incf (gethash ch repo) )
+                         (setf (gethash ch repo) 1)))
+                 (print  (cdr c)))
+          )
+        candidates)
+      repo)))
+
+(defun most-likely-to-be (candidates pattern) 
+  (let ((top 0) (letter nil)) 
+    (progn
+      (maphash 
+        (lambda(k v)
+          (if (> v top) 
+            (setf top v letter k))) 
+        (all-letter-rates (filter-all candidates pattern)))
+      letter)))
